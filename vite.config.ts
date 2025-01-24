143be44da94f3f7e705f2bfde8b7b42980c5d704
guigasprogramador <guigasprogramador@gmail.com>
2025-01-24 16:10:45 -0300

    adição de novos arquivos

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/",
   server: {
     port: 8080,
   },
@@ -11,6 +12,17 @@ export default defineConfig({
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
+    },
+  },
+  build: {
+    outDir: "dist",
+    sourcemap: true,
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          vendor: ['react', 'react-dom', 'react-router-dom'],
+        },
+      },
     },
   },
 });
